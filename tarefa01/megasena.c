ffd5d028842a8790087519d9f1e6a8253c78c82b
Guilherme <g170927@dac.unicamp.br>
2020-10-02 21:26:17 -0300

    cria exercicio megasena.c

---
+++ tarefa01/megasena.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#define COLUNAS 60
+#define MAX_APOSTAS 15
+#define MAX_PESSOAS 1000
+void le_matriz(int apostas[][COLUNAS], int m){
+    int i, j;
+    for(i = 0; i < m; i++){
+        for(j = 0; j < COLUNAS; j++){
+            scanf("%d", &apostas[i][j]);
+            if(apostas[i][j] == 1){
+                apostas[i][j] = j + 1;
+            }
+        }
+    }
+}
+
+void limpar_matriz(int matriz[][6], int m) {
+    int i, j;
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < 15; j++)  {
+            matriz[i][j] = 0;
+        }
+    }
+}
+
+void separa_apostas(int apostas[][COLUNAS], int corretos[MAX_PESSOAS], int m, int sorteados[6]){
+    int i, j, k, l = 0, certos = 0;
+    for (i = 0; i < m; i++){
+        certos = 0;
+        for (j = 0; j < 6; j++){
+            for ( k = 0; k < 60; k++){
+                if(sorteados[j] == apostas[i][k]){
+                    certos += 1;
+                    corretos[l] = certos;
+                }
+            }
+        }
+        l++;
+    }
+}
+
+void define_premios(int corretos[MAX_PESSOAS], double premios[MAX_PESSOAS], int m, double n){
+    int i, j;
+    int aux = 0;
+    for (i = 0; i < m; i++){
+        aux = 0;
+        if(corretos[i] >= 0 && corretos[i] < 4){
+            printf("0.00\n");
+        }
+        else if(corretos[i] == 6){
+            for(j = 0; j < m; j++){
+                if(corretos[i] == corretos[j]){
+                    aux++;
+                }
+            }
+            printf("%0.2f\n", (n * (62.0/100))/aux);
+        }
+        else if(corretos[i] == 5 || corretos[i] == 4){
+            for(j = 0; j < m; j++){
+                if(corretos[i] == corretos[j]){
+                    aux++;
+                }
+            }
+            printf("%0.2f\n", (n * (19.0/100))/aux);
+        }
+    }
+}
+
+int main(){
+    int m, i;
+    double n;
+    int sorteados[6], corretos[MAX_PESSOAS];
+    double premios[MAX_PESSOAS];
+    scanf("%d %lf", &m, &n);
+    int apostas[MAX_PESSOAS][COLUNAS];
+    le_matriz(apostas, m);
+    for(i = 0; i < 6; i++){
+        scanf("%d", &sorteados[i]);
+    }
+    separa_apostas(apostas, corretos, m, sorteados);
+    define_premios(corretos, premios, m, n);
+    return 0;
+}
+
